9822a2577a2cf5650c06510760080093b826676a
hanserHe <94189786@qq.com>
2018-04-09 10:00:30 +0800

    first

---
+++ HSNetWorking/HSNetWorking/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HSNetWorking
+//
+//  Created by Hanser on 2018/4/9.
+//  Copyright © 2018 Mr.H. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
